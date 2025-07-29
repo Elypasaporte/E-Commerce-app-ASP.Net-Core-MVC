14c81d50a0bc18001d065d748787eeffef757ec4
Eliezer Pasaporte <eliezer.pasaporte@yahoo.com>
2025-07-29 21:41:15 +0800

    Update Program.cs

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -55,13 +55,13 @@ builder.Services.AddScoped<IEmailSender, EmailSender>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-//if (!app.Environment.IsDevelopment())
-//{
-//    app.UseExceptionHandler("/Home/Error");
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//    app.UseHsts();
-//}
-app.UseDeveloperExceptionPage();
+   app.UseHsts();
+}
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 StripeConfiguration.ApiKey = builder.Configuration.GetSection("Stripe:SecretKey").Get<string>();
